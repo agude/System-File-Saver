367914d79e8dacccbca447f63f2877a26c261d80
Alexander Gude <alex.public.account+systemfilesaver@gmail.com>
2019-08-30 20:40:32 -0700

    Change license to MIT

--- system_file_saver/system_file_saver.py
+++ system_file_saver/system_file_saver.py
@@ -1,26 +1,5 @@
 #!/usr/bin/env python3
 
-#  Copyright (C) 2013  Alexander Gude -
-#  alex.public.account+systemfilesaver@gmail.com
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-#
-#  The most recent version of this program is available at:
-#  https://github.com/agude/System-File-Saver
-
-
 from sys import exit  # Cleanly exit program
 from subprocess import call  # Access external programs
 from socket import gethostname  # Get the computer's name
